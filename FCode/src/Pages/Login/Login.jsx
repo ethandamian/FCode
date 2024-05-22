027dab5c4fc26a0be26d29e7253d7136353eb523
MeliLiz <melifernandez@ciencias.unam.mx>
2024-05-22 16:29:18 -0600

    Login

--- FCode/src/Pages/Login/Login.jsx
+++ FCode/src/Pages/Login/Login.jsx
@@ -1,10 +1,29 @@
 import "./Login.css"
+import rocket from "../../Images/rocket.png"
 
 export default function Login(){
     return (
-        <div>
-            <h1>Login</h1>
-            <p>This is the Login page</p>
+        <div className="container">
+            <div className="row">
+                <div className="col-6 d-flex justify-content">
+                    <img src={rocket} className="img-rocket"/>
+                </div>
+                <div className="col-6 info">
+                    <p className="txt">¡Únete a la comunidad de alumnos que buscan las mejores oportunidades laborales!</p>
+                    <form>
+                        <div className="mb-3">
+                            <label for="InputEmail" className="d-flex justify-content">Email</label>
+                            <input type="email" className="form-control" id="InputEmail" aria-describedby="emailHelp"/>
+                        </div>
+                        <div className="mb-3">
+                            <label for="InputPassword1" className="d-flex justify-content">Contraseña</label>
+                            <input type="password" className="form-control" id="InputPassword"/>
+                        </div>
+                        <button type="submit" className="btn btn-login">Login</button>
+                    </form>
+                </div>
+            </div>
+            
         </div>
     )
 }
--- FCode/src/Root/Navigation/Navigation.jsx
+++ FCode/src/Root/Navigation/Navigation.jsx
@@ -1,44 +1,41 @@
 // Navigation.jsx
 import React from "react";
+import logo from "./../../Images/logo.png"
+import { NavLink } from "react-router-dom";
 import "./Navigation.css";
+
 
 export default function Navigation() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark barra">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand"><img src={logo}/></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarColor01">
                     <ul className="navbar-nav me-auto">
                         <li className="nav-item">
-                            <a className="nav-link active" href="#">Home <span className="visually-hidden">(current)</span></a>
+                            <NavLink className="nav-link active" href="#">Home <span className="visually-hidden">(current)</span></NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <NavLink className="nav-link" href="#">Recursos</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                            <NavLink className="nav-link" href="#">Comunidad</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
+                            <NavLink className="nav-link" href="#">Proyectos</NavLink>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                            <div className="dropdown-menu">
-                                <a className="dropdown-item" href="#">Action</a>
-                                <a className="dropdown-item" href="#">Another action</a>
-                                <a className="dropdown-item" href="#">Something else here</a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="#">Separated link</a>
-                            </div>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" href="#">Eventos</NavLink>
                         </li>
                     </ul>
                     <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-outline-secondary" type="submit">Search</button>
+                        <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn text-white" type="submit"><i class="bi bi-search"></i></button>
                     </form>
+                    <NavLink ><button className="btn btn-outline-light ms-3">Sign up</button></NavLink>
                 </div>
             </div>
         </nav>
